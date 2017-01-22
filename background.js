26c34dd4211427041183e722aea2e853c72e71fd
Patrick Lai <patricklai10123@gmail.com>
2017-01-22 08:59:46 -0500

    add myo stuff

--- background.js
+++ background.js
@@ -1,8 +1,39 @@
 // background.js
 
 // Called when the user clicks on the browser action.
+console.log(chrome.extension.getURL('pitch.txt'));
+console.log(chrome.runtime.getURL("pitch.txt"));
+
+ var pitchValue = 0;
+
+window.setInterval(function(){
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', chrome.extension.getURL('pitch.txt'), true);
+  xhr.onreadystatechange = function()
+  {
+      if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
+      {
+          //alert(xhr.responseText);
+          pitchValue = parseInt(xhr.responseText);
+          console.log(xhr.responseText);
+      }
+  };
+  xhr.send();
+ 
+
+  if(pitchValue > 11){
+     console.log("value found!");
+     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    });
+  }
+}, 1000);
+
+
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
+  // Send a message to the active tabs
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
--- content.js
+++ content.js
@@ -1,19 +1,43 @@
 // content.js
- 
+
+
+
+
+
+
+
 
 var filesArray = [];
 for(var i=1; i<=27; i++){
 	filesArray.push('http://www.cs.mcgill.ca/~plai15/img/images/' + 'img' + i +'.jpg');
 }
 
- // var images = document.getElementsByTagName('img');-
- // for (var i = 0, l = images.length; i < l; i++) {
-	// 		images[i].src = 'https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg';
-	// 	}
-	
+
+//the output of the hellomyo file is a text file that is stored in the pitch variable. Find a way to read the number from it
+
+
+
+
+
+
+
+
+
+// window.setInterval(function(){
+//   var imgCount = filesArray.length;
+//   		if(pitchValue > 14 || pitchValue < -14){
+//   			var images2 = document.getElementsByTagName('img');
+//  			for (var i = 0, l = images2.length; i < l; i++) {
+// 				var randomIndex = Math.floor(Math.random() * (imgCount-1));
+// 				images2[i].src = filesArray[randomIndex];
+// 			}
+//   		}
+    		
+// }, 1000);
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-
+  	console.log("----ez");
 	if( request.message === "clicked_browser_action" ) {
 			var imgCount = filesArray.length;
     		var images2 = document.getElementsByTagName('img');
